c653f7be805ea3ba8e06068ab2825d1c3b5c26f5
Tranxuanvu <xuanvu442@gmail.com>
2015-03-07 10:57:48 +0700

    add data for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# create fake data env for development
+unless Rails.env.production?
+  20.times do
+    FactoryGirl.create(:product)
+  end
+
+  Product.create(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph,
+    prices: Faker::Commerce.price,
+    categories: Faker::Lorem.paragraph,
+    image: Faker::Avatar.image
+  )
+end
