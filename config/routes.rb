6daf8f25e9b8de1d6684f86106ee89c82094fe4d
Tranxuanvu <xuanvu442@gmail.com>
2015-03-07 12:22:16 +0700

    create URLs paginate

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
+  concern :paginatable do
+    get 'page/:page', :action => :index, :on => :collection, :as => ''
+  end
+
+  resources :products, :concerns => :paginatable
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
