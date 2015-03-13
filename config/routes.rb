ed02d9dcff4bd8518d1e0173f36bd5ab85340aa6
Tranxuanvu <xuanvu442@gmail.com>
2015-03-13 21:07:17 +0700

    admin pages with activeadmin gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   concern :paginatable do
     get 'page/:page', :action => :index, :on => :collection, :as => ''
   end
